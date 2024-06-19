83cbad5aa04b3358e9c7c4b504eda593c5a1c190
yousefsobhy12 <yousefsobhy124@gmail.com>
2024-06-19 22:27:48 +0300

    add note bottom sheet created

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/add_note_bottom_sheet.dart';
 import 'package:notes_app/views/widgets/home_view_body.dart';
 
 class HomeView extends StatelessWidget {
@@ -8,10 +9,18 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              builder: (context) {
+                return const AddNoteBottomSheet();
+              });
+        },
         child: const Icon(Icons.add_rounded),
       ),
       body: const HomeViewBody(),
     );
   }
 }
+
+
