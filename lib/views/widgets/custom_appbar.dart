307d8397382bc6efe55da795ed05f6e4496cb0c7
yousefsobhy12 <yousefsobhy124@gmail.com>
2024-06-21 15:57:54 +0300

    hive package added

--- lib/views/widgets/custom_appbar.dart
+++ lib/views/widgets/custom_appbar.dart
@@ -14,7 +14,7 @@ class CustomAppbar extends StatelessWidget {
       children: [
         Text(
           title,
-          style: TextStyle(
+          style: const TextStyle(
             fontSize: 28,
             fontWeight: FontWeight.bold,
           ),
