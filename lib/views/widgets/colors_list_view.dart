129e91b712165ef6c54d68df72d1fc0333e4bd21
yousefsobhy12 <yousefsobhy124@gmail.com>
2024-06-27 02:24:52 +0300

    colors palette added

--- lib/views/widgets/colors_list_view.dart
+++ lib/views/widgets/colors_list_view.dart
@@ -1,31 +1,68 @@
 import 'package:flutter/material.dart';
 
 class ColorItem extends StatelessWidget {
-  const ColorItem({super.key});
-
+  const ColorItem({super.key, required this.activated, required this.color});
+  final bool activated;
+  final Color color;
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(2),
-      child: CircleAvatar(
-        radius: 30,
-      ),
+    return Padding(
+      padding: const EdgeInsets.all(2),
+      child: activated
+          ? const CircleAvatar(
+              radius: 29,
+              backgroundColor: Colors.white,
+              child: CircleAvatar(
+                radius: 28,
+              ),
+            )
+          : CircleAvatar(
+              radius: 30,
+              backgroundColor: color,
+            ),
     );
   }
 }
 
-class ColorsListView extends StatelessWidget {
+class ColorsListView extends StatefulWidget {
   const ColorsListView({super.key});
 
+  @override
+  State<ColorsListView> createState() => _ColorsListViewState();
+}
+
+class _ColorsListViewState extends State<ColorsListView> {
+  int currentIndex = 0;
+  List<Color> colors = const [
+    Color(0xffA1E8AF),
+    Color(0xff94C595),
+    Color(0xff747C92),
+    Color(0xff372772),
+    Color(0xff3A2449),
+    Color(0xffEDD2E0),
+    Color(0xffEDBBB4),
+    Color(0xffDBABBE),
+    Color(0xffBAA1A7),
+    Color(0xff797B84),
+  ];
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 38 * 2,
       child: ListView.builder(
           scrollDirection: Axis.horizontal,
-          itemCount: 10,
-          itemBuilder: (builder, context) {
-            return const ColorItem();
+          itemCount: colors.length,
+          itemBuilder: (builder, index) {
+            return GestureDetector(
+              onTap: () {
+                currentIndex = index;
+                setState(() {});
+              },
+              child: ColorItem(
+                activated: currentIndex == index,
+                color: colors[index],
+              ),
+            );
           }),
     );
   }
